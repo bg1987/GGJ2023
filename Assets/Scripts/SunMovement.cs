7efa6d9ca294e7613b2a75d6a7eead779b78bf6d
Benny Gafanvoich <bg1987@gmail.com>
2023-02-04 02:07:22 +0200

    good sun movement

--- Assets/Scripts/SunMovement.cs
+++ Assets/Scripts/SunMovement.cs
@@ -9,8 +9,11 @@ public class SunMovement : MonoBehaviour
 {
     private Transform sun;
 
+    public bool rotateY = true;
     public float yAxisRotationTime = 10f;
-    
+
+
+    public bool rotateX = true;
     public float xAxisRotationTime = 10f;
 
     public float maxXRotation = 10f;
@@ -18,23 +21,69 @@ public class SunMovement : MonoBehaviour
     public bool updateTimes = false;
 
     private LTDescr xTween;
-
+ 
     private LTDescr yTween;
     // Start is called before the first frame update
     void Start()
     {
         sun = transform;
         sun.eulerAngles = new Vector3(maxXRotation, 0, 0);
-        // xTween = LeanTween.rotateX(gameObject, 180f - maxXRotation, xAxisRotationTime).setRepeat(-1);
-        yTween = LeanTween.rotateY(gameObject, 3600, yAxisRotationTime);
+        if (rotateX)
+        {
+            RotateX();
+        }
+
+        if (rotateY)
+        {
+            RotateY();
+        }
+    }
+
+    private void RotateY()
+    {
+        yTween = LeanTween.rotateY(gameObject, 720, yAxisRotationTime).setRepeat(-1);
+    }
+
+    private void RotateX()
+    {
+        xTween = LeanTween.rotateX(gameObject, 180f - maxXRotation, xAxisRotationTime).setRepeat(-1);
     }
 
     private void Update()
     {
         if (updateTimes)
         {
-            yTween.setTime(yAxisRotationTime);
-            if(xTween != null)            xTween.setTime(xAxisRotationTime);
+            if (rotateX)
+            {
+                if (xTween == null)
+                {
+                    RotateX();
+                }
+
+                xTween.resume();
+                xTween.setTime(xAxisRotationTime);
+            }
+            else
+            {
+                xTween.pause();
+            }
+
+            if (rotateY)
+            {
+                if (yTween == null)
+                {
+                    RotateY();
+                }
+
+                yTween.resume();
+                yTween.setTime(yAxisRotationTime);
+            }
+            else
+            {
+                yTween.pause();
+            }
+            
+            
         }
     }
 }
